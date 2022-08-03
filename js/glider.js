00be3f898c9e9395c386a7020f1506050c1b3843
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-08-03 19:23:45 +0300

    Улучшил дизайн

--- js/glider.js
+++ js/glider.js
@@ -1,6 +1,12 @@
 import Glide from '@glidejs/glide'
 
 new Glide('.glide', { 
+  type: 'carousel',
   autoplay: 3000,
-  perView: 4
+  perView: 4,
+  breakpoints: {
+    700: {
+      perView: 1
+    }
+  }
 }).mount()
