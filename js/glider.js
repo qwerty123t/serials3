0207bb18654e19e225712464cfc50e2f87d5cb2b
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-08-04 11:31:12 +0300

    Адаптировал слайдер для смартфонов

--- js/glider.js
+++ js/glider.js
@@ -1,12 +1,26 @@
 import Glide from '@glidejs/glide'
 
+
+function vw(v) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  return (v * w) / 100;
+}
+
+
 new Glide('.glide', { 
   type: 'carousel',
   autoplay: 3000,
   perView: 4,
+  gap: `${vw(2.85714286)}`,
   breakpoints: {
     700: {
-      perView: 1
+      perView: 1,
+      peek: {
+        before: 0,
+        after: `${vw(8.5714286)}vw`
+      }
     }
   }
 }).mount()
+
+
